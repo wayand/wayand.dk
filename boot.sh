700638484a93531c88cec24b5a715b9da199f5c5
wayand bahramzy <lawangjan@hotmail.com>
2023-08-16 00:02:18 +0200

    added SEO Meta Fields and new flask version with packages upgraded

--- boot.sh
+++ boot.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
 # this script is used to boot a Docker container
 cd /var/www/html
 echo "I am Here.........starting while"
 pip3 install -r requirements.txt
-# while true; do
-#     flask db upgrade
-#     if [[ "$?" == "0" ]]; then
-#         break
-#     fi
-#     echo Deploy command failed, retrying in 5 secs...
-#     sleep 5
-# done
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
 
 
 echo "I am Here.........DONE while"
